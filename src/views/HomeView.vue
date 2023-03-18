21c5d06a25660bfe02fabfad76c03a914ba138a5
OlivMagno <olivmagno1@gmail.com>
2023-03-17 22:30:25 -0300

    selecting redacao by id

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,20 @@ import { ref, onMounted } from "vue";
 
 const tabela = ref("");
 const tabelaPage = ref(0);
+const redacaoSelect = ref(-1);
+const redacaoZoom = ref("");
+
+const BuscaRedacao = async (red_id) => {
+  const res = await fetch(`https://desafio.pontue.com.br/redacao/${red_id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "authorization, x-requested-with",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  }).then((res) => res.json());
+  redacaoZoom.value = res.data;
+};
 
 const BuscaRedAluno = async () => {
   const res = await fetch(
@@ -17,7 +31,11 @@ const BuscaRedAluno = async () => {
     }
   ).then((res) => res.json());
   tabela.value = res.data;
-  console.log(tabela);
+};
+
+const checkActual = (index) => {
+  if (redacaoSelect.value === index) return true;
+  else return false;
 };
 
 const checkFirstPage = () => {
@@ -48,38 +66,64 @@ onMounted(() => {
 
 <template>
   <h1>Página de entrada</h1>
+  <h2>Redação selecionada: {{ redacaoSelect }}</h2>
+  <div class="header">
+    <p>Número</p>
+    <p>Data</p>
+  </div>
   <div
     class="table"
     v-for="(entry, index) in tabela.slice(tabelaPage, tabelaPage + 10)"
+    @click="
+      () => {
+        redacaoSelect = index + tabelaPage;
+        BuscaRedacao(entry.id);
+      }
+    "
     :key="index"
+    :class="{ chosen: checkActual(index + tabelaPage) }"
   >
-    <p>{{ entry.id }}</p>
     <p>{{ entry.numero }}</p>
     <p>{{ entry.created_at }}</p>
   </div>
   <div class="nav">
-    <button :class="{ off: checkFirstPage }" @click="prevPage">
+    <button :class="{ off: checkFirstPage() }" @click="prevPage">
       Página anterior
     </button>
-    <h2>{{ tabelaPage }}</h2>
-    <button :class="{ off: checkLastPage }" @click="nextPage">
+    <h2>{{ tabelaPage / 10 + 1 }}</h2>
+    <button :class="{ off: checkLastPage() }" @click="nextPage">
       Página seguinte
     </button>
+  </div>
+  <div class="zoom">
+    <p>{{ redacaoZoom }}</p>
   </div>
 </template>
 
 <style scoped>
-.table {
+.table,
+.header {
   display: flex;
   flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  width: 30rem;
 }
 
 .nav {
+  position: absolute;
+  bottom: 5rem;
   display: flex;
   flex-flow: row nowrap;
 }
 
 .off {
-  opacity: 0;
+  opacity: 0.5;
+}
+
+.chosen {
+  height: 10rem;
+  background-color: black;
+  color: white;
 }
 </style>
