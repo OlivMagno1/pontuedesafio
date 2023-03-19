874170ec0d6ba5d764ca6fc3d7ebd8feda0a4979
OlivMagno <olivmagno1@gmail.com>
2023-03-19 00:17:49 -0300

    added color palette

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,54 @@
 </template>
 
 <style>
+:root {
+  --primary: #532e94;
+  --secondary: #403b44;
+  --text: #272727;
+  --accent: #cf007f;
+  --purple: #482683;
+  --cyan: #35b9c8;
+  --orange: #ed8a26;
+  --clear0: #ffffff;
+  --clear1: #efeef4;
+  --clear2: #dddce6;
+  --clear3: #b8b6c2;
+  --dark: #000000;
+  --teal: #2f98bd;
+}
+
+@font-face {
+  font-family: Museo;
+  src: url("./assets/fonts/MuseoSans-100.otf");
+  font-weight: 100;
+}
+
+@font-face {
+  font-family: Museo;
+  src: url("./assets/fonts/MuseoSans-300.otf");
+  font-weight: 300;
+}
+
+@font-face {
+  font-family: Museo;
+  src: url("./assets/fonts/MuseoSans-500.otf");
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: Museo;
+  src: url("./assets/fonts/MuseoSans-700.otf");
+  font-weight: 700;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Museo", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  justify-content: center;
-
-  width: 100vw;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
 }
 </style>
